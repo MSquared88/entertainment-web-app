0957355e6626ddfc1300e19fe5312cf006d1a719
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-13 17:55:52 -0500

    added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        red: "#FC4747",
+        blue: {
+          dark: "#10141E",
+          semi: "#161D2F",
+          grayish: "#5A698F",
+        },
+      },
+    },
   },
   plugins: [],
 };
