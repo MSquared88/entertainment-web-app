b025c2caf439021567cd1ea824931a0956e1aa84
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-15 15:57:16 -0500

    added getbookmars to media model

--- app/models/media.server.ts
+++ app/models/media.server.ts
@@ -1,5 +1,18 @@
-import type { Media } from "@prisma/client";
+import type { User, Media } from "@prisma/client";
 
 import { prisma } from "~/db.server";
 
-export type { Media } from "@prisma/client";
+export async function getUserBookmarks(userId: User["id"]) {
+  return prisma.media.findMany({
+    where: {
+      userIds: {
+        some: {
+          userId: userId,
+        },
+      },
+    },
+  });
+}
+export async function getMediaListItems(userId: User["id"]) {
+  return prisma.media.findMany();
+}
