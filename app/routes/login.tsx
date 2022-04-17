69d834e70cd5557fca52498e9dce6afb59e65934
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-16 19:12:45 -0500

    changed login to redirect to media/all route

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -65,7 +65,7 @@ export const action: ActionFunction = async ({ request }) => {
     request,
     userId: user.id,
     remember: remember === "on" ? true : false,
-    redirectTo: typeof redirectTo === "string" ? redirectTo : "/media",
+    redirectTo: typeof redirectTo === "string" ? redirectTo : "/media/all",
   });
 };
 
@@ -77,7 +77,7 @@ export const meta: MetaFunction = () => {
 
 export default function LoginPage() {
   const [searchParams] = useSearchParams();
-  const redirectTo = searchParams.get("redirectTo") || "/media";
+  const redirectTo = searchParams.get("redirectTo") || "/media/all";
   const actionData = useActionData() as ActionData;
   const emailRef = React.useRef<HTMLInputElement>(null);
   const passwordRef = React.useRef<HTMLInputElement>(null);
