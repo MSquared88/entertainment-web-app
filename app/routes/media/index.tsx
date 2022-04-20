36021b127552cb75d48860375af541db4abac623
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-20 15:57:41 -0500

    removed unused exports

--- app/routes/media/index.tsx
+++ app/routes/media/index.tsx
@@ -1,44 +1,11 @@
-import type { LoaderFunction } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Form, Outlet, useLoaderData } from "@remix-run/react";
+import { Outlet } from "@remix-run/react";
 
 //db
-import { getMediaListItems } from "~/models/media.server";
-
-//components
-import ListOfMediaDisplay from "~/components/listOfMedia";
-type LoaderData = {
-  mediaListItems: Awaited<ReturnType<typeof getMediaListItems>>;
-};
-
-export const loader: LoaderFunction = async ({ request }) => {
-  const mediaListItems = await getMediaListItems();
-  return json<LoaderData>({ mediaListItems });
-};
 
 export default function MediaPage() {
-  const { mediaListItems } = useLoaderData() as LoaderData;
-
   return (
     <div className="flex  w-full">
       <Outlet />
-      {/* <div>
-        <Form>
-          <button className="c" type="submit">
-            <img src="/assets/icon-search.svg" alt="" />
-          </button>
-          <input
-            type="text"
-            id="search-input"
-            name="search"
-            placeholder={`search for something`}
-            className="text- mb-4 h-8 w-96 border-b bg-blue-dark text-white placeholder:focus:text-blue-dark"
-          />
-        </Form>
-      </div>
-      <main className="flex h-full bg-blue-dark">
-        <ListOfMediaDisplay mediaListItems={mediaListItems} />
-      </main> */}
     </div>
   );
 }
