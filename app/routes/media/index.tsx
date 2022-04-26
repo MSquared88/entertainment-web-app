a46124d5a2c5c532635691495fc839ab9929ca22
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-26 17:06:54 -0500

    removed styles from index media file

--- app/routes/media/index.tsx
+++ app/routes/media/index.tsx
@@ -4,8 +4,8 @@ import { Outlet } from "@remix-run/react";
 
 export default function MediaPage() {
   return (
-    <div className="flex  w-full">
+    <>
       <Outlet />
-    </div>
+    </>
   );
 }
