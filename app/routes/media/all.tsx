22803f0c655521b6716422ba08eb49566d43cf6c
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-04-26 17:06:20 -0500

    added search functionality to all media route

--- app/routes/media/all.tsx
+++ app/routes/media/all.tsx
@@ -3,7 +3,7 @@ import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 //db
-import { getMediaListItems } from "~/models/media.server";
+import { getMediaListItems, searchAll } from "~/models/media.server";
 
 //components
 import { SearchForm } from "~/components/searchForm";
@@ -12,24 +12,50 @@ import ListOfMediaDisplay from "~/components/listOfMedia";
 type LoaderData = {
   mediaListItems: Awaited<ReturnType<typeof getMediaListItems>>;
 };
+type SearchData = {
+  searchReturn: Awaited<ReturnType<typeof searchAll>>;
+};
 
 export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams.get("search");
+  if (searchParams) {
+    const searchReturn = await searchAll(searchParams);
+
+    return json<SearchData>({ searchReturn });
+  }
+
   const mediaListItems = await getMediaListItems();
   return json<LoaderData>({ mediaListItems });
 };
 
 export default function MediaPage() {
   const { mediaListItems } = useLoaderData() as LoaderData;
+  const { searchReturn } = useLoaderData() as SearchData;
+  console.log(
+    "🚀 ~ file: all.tsx ~ line 39 ~ MediaPage ~ searchReturn",
+    searchReturn
+  );
 
   return (
-    <div className="flex h-[88%] flex-col bg-blue-dark">
+    <div className="fixed flex flex-col bg-blue-dark lg:mt-12">
       <SearchForm placeHolder={"Search for movies or TV series"} />
-      <main className=" h-full bg-blue-dark">
-        <ListOfMediaDisplay mediaListItems={mediaListItems}>
-          <div>trending</div>
-          <div>recomened</div>
-        </ListOfMediaDisplay>
-      </main>
+      <div className="bg-blue-dark">
+        <div>trending</div>
+        <h1 className="pb-4 text-3xl text-white">Recommended for you</h1>
+        {searchReturn ? (
+          <ListOfMediaDisplay
+            mediaListItems={searchReturn}
+          ></ListOfMediaDisplay>
+        ) : (
+          <ListOfMediaDisplay
+            mediaListItems={mediaListItems}
+          ></ListOfMediaDisplay>
+        )}
+      </div>
+    </div>
+  );
+}
 export function ErrorBoundary({ error }: { error: Error }) {
   return (
     <div className="bg-pink-200 text-red">
