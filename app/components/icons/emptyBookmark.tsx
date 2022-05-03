25bf063d147fb63f5b8e3741355eed81eb95648e
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-05-02 20:18:07 -0500

    work on bookmarks route

--- app/components/icons/emptyBookmark.tsx
+++ app/components/icons/emptyBookmark.tsx
@@ -1,32 +1,31 @@
 import { Form } from "@remix-run/react";
-import type { ActionFunction } from "@remix-run/node";
 
-import { getUserId } from "~/session.server";
-import { redirect } from "@remix-run/node";
+import type { Media } from "@prisma/client";
 
-export const action: ActionFunction = async ({ request }) => {
-  const formData = await request.formData();
-  const userId = await getUserId(request);
-};
+import { motion } from "framer-motion";
 
-export default function EmptyBookmark() {
+export default function EmptyBookmark({ media }: { media: Media }) {
   return (
     <Form method="post">
-      <button
-        type="submit"
+      <input type="hidden" name="mediaId" defaultValue={media.id} hidden />
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
         name="action"
+        value="add-bookmark"
+        type="submit"
         className="group absolute top-8 right-8 rounded-full bg-blue-dark p-3 opacity-75 outline-none outline-hidden hover:bg-white hover:opacity-100 hover:drop-shadow-xl focus:bg-white"
       >
         <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
           <path
             d="m10.518.75.399 12.214-5.084-4.24-4.535 4.426L.75 1.036l9.768-.285Z"
             stroke="#FFF"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             fill="none"
             className="group-hover:stroke-blue-dark group-focus:stroke-blue-dark"
           />
         </svg>
-      </button>
+      </motion.button>
     </Form>
   );
 }
--- app/routes/media/bookmarks.tsx
+++ app/routes/media/bookmarks.tsx
@@ -1,3 +1,79 @@
-export default function () {
-  return <div>bookmarks</div>;
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+import { SearchForm } from "~/components/searchForm";
+
+//db
+import { searchUserBookmarks, getUserBookmarks } from "~/models/media.server";
+import { requireUserId } from "~/session.server";
+
+//components
+import ListOfMediaDisplay from "~/components/listOfMedia";
+
+type LoaderData = {
+  bookMarkedMedia: Awaited<ReturnType<typeof getUserBookmarks>>;
+};
+type SearchData = {
+  searchReturn: Awaited<ReturnType<typeof searchUserBookmarks>>;
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const searchParams = url.searchParams.get("search");
+  const userId = await requireUserId(request);
+
+  if (searchParams) {
+    const searchReturn = await searchUserBookmarks(userId, searchParams);
+
+    return json<SearchData>({ searchReturn });
+  }
+
+  const bookMarkedMedia = await getUserBookmarks(userId);
+  return json<LoaderData>({ bookMarkedMedia });
+};
+
+export default function MediaPage() {
+  const { bookMarkedMedia } = useLoaderData() as LoaderData;
+  const { searchReturn } = useLoaderData() as SearchData;
+
+  return (
+    <div className=" flex flex-col bg-blue-dark lg:mt-12">
+      <SearchForm placeHolder={"Search for mookmarked shows"} />
+
+      <div className=" bg-blue-dark">
+        {searchReturn ? (
+          <ListOfMediaDisplay
+            mediaListItems={searchReturn}
+          ></ListOfMediaDisplay>
+        ) : (
+          <div>
+            <h1>Bookmarked Movies</h1>
+            <ListOfMediaDisplay
+              mediaListItems={bookMarkedMedia.filter((media) => {
+                return media.category === "Movie";
+              })}
+            ></ListOfMediaDisplay>
+            <h1>Bookmarked Series</h1>
+
+            <ListOfMediaDisplay
+              mediaListItems={bookMarkedMedia.filter((media) => {
+                return media.category === "TV Series";
+              })}
+            ></ListOfMediaDisplay>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <div className="mt-36 bg-pink-200 p-10 text-red">
+      <h1>Error</h1>
+      <p>{error.message}</p>
+      <p>The stack trace is:</p>
+      <pre>{error.stack}</pre>
+    </div>
+  );
 }
