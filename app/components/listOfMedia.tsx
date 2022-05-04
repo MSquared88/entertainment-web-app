f94c95b070260be3da7435053637c95dccf1f5d3
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-05-03 21:13:37 -0500

    conditional render appropriate bookmark component

--- app/components/listOfMedia.tsx
+++ app/components/listOfMedia.tsx
@@ -1,33 +1,48 @@
 import MediaInfo from "./mediaInfo";
 import type { Media } from "@prisma/client";
 
+import { motion } from "framer-motion";
+
+import EmptyBookmark from "~/components/icons/emptyBookmark";
+import FullBookmark from "~/components/icons/fullBookmark";
+
 export default function ListOfMediaDisplay({
   mediaListItems,
+  userBookmarksIds,
   children,
 }: {
   mediaListItems: Media[];
+  userBookmarksIds?: string[];
   children?: React.ReactChild | React.ReactChild[];
 }) {
   return (
     <div className="flex w-full flex-col">
       {children}
       {
-        <ul className="scrollbar  flex h-[78vh] flex-wrap justify-start overflow-y-scroll">
+        <ul className="flex h-[78vh] flex-wrap items-center gap-4">
           {mediaListItems.map((media) => (
-            <li
+            <motion.li
+              whileHover={{ scale: 1.03 }}
               key={media.id}
               className="mx-4 my-14 max-h-[223px] max-w-[280] text-white"
             >
-              <img
-                alt={media.title}
-                src={media.mediumThumbnail}
-                className="max-w-sm rounded-xl border-blue-dark p-1"
-              />
+              <div className="relative">
+                <img
+                  alt={media.title}
+                  src={media.mediumThumbnail}
+                  className="max-w-sm rounded-xl border-blue-dark p-1"
+                />
+                {userBookmarksIds?.includes(media.id) ? (
+                  <FullBookmark media={media} />
+                ) : (
+                  <EmptyBookmark media={media} />
+                )}
+              </div>
               <div className="flex">
                 <MediaInfo media={media} />
               </div>
               <h1 className="text-2xl font-bold">{media.title}</h1>
-            </li>
+            </motion.li>
           ))}
         </ul>
       }
