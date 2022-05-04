6d88e74dd9d935fd2cd5b868c3eff312051a2c17
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-05-03 21:12:46 -0500

    remove bookmark functionality added to fullBookmark component

--- app/components/icons/fullBookmark.tsx
+++ app/components/icons/fullBookmark.tsx
@@ -1,12 +1,31 @@
-export default function FullBookmark() {
+import { Form } from "@remix-run/react";
+
+import type { Media } from "@prisma/client";
+
+import { motion } from "framer-motion";
+
+export default function FullBookmark({ media }: { media: Media }) {
   return (
-    <div className="absolute top-8 right-8 rounded-full bg-black p-2 opacity-50">
-      <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
-          fill="#FFF"
-        />
-      </svg>
-    </div>
+    <Form method="post">
+      <input type="hidden" name="mediaId" defaultValue={media.id} hidden />
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+        name="action"
+        value="remove-bookmark"
+        type="submit"
+        className="group absolute top-8 right-8 rounded-full bg-blue-dark p-3 opacity-75 outline-none outline-hidden hover:bg-white hover:opacity-100 hover:drop-shadow-xl focus:bg-white"
+      >
+        <svg width="12" height="14" xmlns="http://www.w3.org/2000/svg">
+          <path
+            stroke="#FFF"
+            strokeWidth="1.5"
+            fill="#FFF"
+            d="M10.61 0c.14 0 .273.028.4.083a1.03 1.03 0 0 1 .657.953v11.928a1.03 1.03 0 0 1-.656.953c-.116.05-.25.074-.402.074-.291 0-.543-.099-.756-.296L5.833 9.77l-4.02 3.924c-.218.203-.47.305-.756.305a.995.995 0 0 1-.4-.083A1.03 1.03 0 0 1 0 12.964V1.036A1.03 1.03 0 0 1 .656.083.995.995 0 0 1 1.057 0h9.552Z"
+            className="group-hover: group-hover:stroke-blue-dark group-focus:stroke-blue-dark"
+          />
+        </svg>
+      </motion.button>
+    </Form>
   );
 }
