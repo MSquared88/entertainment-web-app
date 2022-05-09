2c8e304f89da57fb4b8746801451d34b732e3411
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-05-09 15:12:17 -0500

    refactored bookmarks route for removing bookmarks

--- app/routes/media/bookmarks.tsx
+++ app/routes/media/bookmarks.tsx
@@ -1,21 +1,74 @@
-import type { LoaderFunction } from "@remix-run/node";
+import type { ActionFunction, LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 
 import { SearchForm } from "~/components/searchForm";
 
 //db
-import { searchUserBookmarks, getUserBookmarks } from "~/models/media.server";
+import {
+  searchUserBookmarks,
+  getUserBookmarks,
+  getUserBookmarksIds,
+  addBookmark,
+  removeBookmark,
+} from "~/models/media.server";
 import { requireUserId } from "~/session.server";
 
 //components
 import ListOfMediaDisplay from "~/components/listOfMedia";
 
+interface ActionData {
+  errors: {
+    mediaId?: string;
+  };
+}
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const mediaId = formData.get("mediaId");
+  const action = formData.get("action");
+  const userId = await requireUserId(request);
+  if (typeof mediaId !== "string") {
+    return json<ActionData>(
+      { errors: { mediaId: "incorrect media id" } },
+      { status: 400 }
+    );
+  }
+  switch (action) {
+    case "add-bookmark":
+      try {
+        const bookmark = await addBookmark(userId, mediaId);
+        return bookmark;
+      } catch (error) {
+        return json<ActionData>(
+          {
+            errors: {
+              mediaId: `something went wrong adding a bookmark to ${mediaId}`,
+            },
+          },
+          { status: 400 }
+        );
+      }
+    case "remove-bookmark":
+      try {
+        const bookmark = await removeBookmark(userId, mediaId);
+        return bookmark;
+      } catch (error) {
+        return json<ActionData>(
+          {
+            errors: {
+              mediaId: `something went wrong removing bookmark for ${mediaId}`,
+            },
+          },
+          { status: 400 }
+        );
+      }
+  }
+};
+
 type LoaderData = {
   bookMarkedMedia: Awaited<ReturnType<typeof getUserBookmarks>>;
-};
-type SearchData = {
-  searchReturn: Awaited<ReturnType<typeof searchUserBookmarks>>;
+  userBookmarksIds: string[];
+  isSearch: Boolean;
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
@@ -23,28 +76,34 @@ export const loader: LoaderFunction = async ({ request }) => {
   const searchParams = url.searchParams.get("search");
   const userId = await requireUserId(request);
 
-  if (searchParams) {
-    const searchReturn = await searchUserBookmarks(userId, searchParams);
+  const userBookmarks = await getUserBookmarksIds(userId);
+  const userBookmarksIds = userBookmarks.map((bookmark) => bookmark.mediaId);
 
-    return json<SearchData>({ searchReturn });
+  //search params
+  let isSearch = false;
+  if (searchParams) {
+    isSearch = true;
+    const bookMarkedMedia = await searchUserBookmarks(userId, searchParams);
+    return json<LoaderData>({ bookMarkedMedia, userBookmarksIds, isSearch });
   }
 
   const bookMarkedMedia = await getUserBookmarks(userId);
-  return json<LoaderData>({ bookMarkedMedia });
+  return json<LoaderData>({ bookMarkedMedia, userBookmarksIds, isSearch });
 };
 
 export default function MediaPage() {
-  const { bookMarkedMedia } = useLoaderData() as LoaderData;
-  const { searchReturn } = useLoaderData() as SearchData;
+  const { bookMarkedMedia, userBookmarksIds, isSearch } =
+    useLoaderData() as LoaderData;
 
   return (
     <div className=" flex flex-col bg-blue-dark lg:mt-12">
       <SearchForm placeHolder={"Search for mookmarked shows"} />
 
       <div className=" bg-blue-dark">
-        {searchReturn ? (
+        {isSearch ? (
           <ListOfMediaDisplay
-            mediaListItems={searchReturn}
+            mediaListItems={bookMarkedMedia}
+            userBookmarksIds={userBookmarksIds}
           ></ListOfMediaDisplay>
         ) : (
           <div>
@@ -53,6 +112,7 @@ export default function MediaPage() {
               mediaListItems={bookMarkedMedia.filter((media) => {
                 return media.category === "Movie";
               })}
+              userBookmarksIds={userBookmarksIds}
             ></ListOfMediaDisplay>
             <h1>Bookmarked Series</h1>
 
@@ -60,6 +120,7 @@ export default function MediaPage() {
               mediaListItems={bookMarkedMedia.filter((media) => {
                 return media.category === "TV Series";
               })}
+              userBookmarksIds={userBookmarksIds}
             ></ListOfMediaDisplay>
           </div>
         )}
