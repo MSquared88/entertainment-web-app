c40c1c2b78d3f250c0cf485b49fce5ce2b45833c
Matthew Meeves <39702456+MSquared88@users.noreply.github.com>
2022-05-11 15:54:40 -0500

    refactored to use mediaItemList component

--- app/components/listOfMedia.tsx
+++ app/components/listOfMedia.tsx
@@ -1,10 +1,6 @@
-import MediaInfo from "./mediaInfo";
 import type { Media } from "@prisma/client";
 
-import { motion } from "framer-motion";
-
-import EmptyBookmark from "~/components/icons/emptyBookmark";
-import FullBookmark from "~/components/icons/fullBookmark";
+import { MediaListItem } from "./mediaListItem";
 
 export default function ListOfMediaDisplay({
   mediaListItems,
@@ -19,30 +15,13 @@ export default function ListOfMediaDisplay({
     <div className="flex w-full flex-col">
       {children}
       {
-        <ul className="flex h-[78vh] flex-wrap items-center gap-4">
+        <ul className="flex h-[78vh] flex-wrap content-start items-center gap-4">
           {mediaListItems.map((media) => (
-            <motion.li
-              whileHover={{ scale: 1.03 }}
+            <MediaListItem
+              userBookmarksIds={userBookmarksIds}
               key={media.id}
-              className="mx-4 my-14 max-h-[223px] max-w-[280] text-white"
-            >
-              <div className="relative">
-                <img
-                  alt={media.title}
-                  src={media.mediumThumbnail}
-                  className="max-w-sm rounded-xl border-blue-dark p-1"
-                />
-                {userBookmarksIds?.includes(media.id) ? (
-                  <FullBookmark media={media} />
-                ) : (
-                  <EmptyBookmark media={media} />
-                )}
-              </div>
-              <div className="flex">
-                <MediaInfo media={media} />
-              </div>
-              <h1 className="text-2xl font-bold">{media.title}</h1>
-            </motion.li>
+              media={media}
+            />
           ))}
         </ul>
       }
